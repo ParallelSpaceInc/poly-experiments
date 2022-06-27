24bada02459b764ffa5781873903a1eda6ff0049
Taehyeong Kim <th.imk3@gmail.com>
2022-06-27 13:48:54 +0900

    impl webxr

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
 import { Box } from "@components/models/Box";
-import { Canvas } from "@react-three/fiber";
+import { VRCanvas } from "@react-three/xr";
 import { useState } from "react";
 
 function Home() {
   const [clicked, click] = useState(false);
   return (
     <div>
-      <Canvas style={{ width: "auto", height: "90vh" }}>
+      <VRCanvas style={{ width: "auto", height: "90vh" }}>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
         <Box click={click} clicked={clicked} position={[-1.2, 0, 0]} />
         <Box click={click} clicked={clicked} position={[1.2, 0, 0]} />
-      </Canvas>
+      </VRCanvas>
       <button
         style={{ width: 100, height: 100 }}
         onClick={() => {
