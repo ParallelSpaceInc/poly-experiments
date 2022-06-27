1d1d9ba59c33a6ad0f5bd2e9d51087458be3a928
Taehyeong Kim <th.imk3@gmail.com>
2022-06-27 13:59:05 +0900

    arcanvas

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
 import { Box } from "@components/models/Box";
-import { VRCanvas } from "@react-three/xr";
+import { VRCanvas as ARCanvas } from "@react-three/xr";
 import { useState } from "react";
 
 function Home() {
   const [clicked, click] = useState(false);
   return (
     <div>
-      <VRCanvas style={{ width: "auto", height: "90vh" }}>
+      <ARCanvas style={{ width: "auto", height: "90vh" }}>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
         <Box click={click} clicked={clicked} position={[-1.2, 0, 0]} />
         <Box click={click} clicked={clicked} position={[1.2, 0, 0]} />
-      </VRCanvas>
+      </ARCanvas>
       <button
         style={{ width: 100, height: 100 }}
         onClick={() => {
