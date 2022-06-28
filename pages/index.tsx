7897ba50e6eaf7f635512cc625f6511b42a5cb28
Taehyeong Kim <th.imk3@gmail.com>
2022-06-28 11:23:42 +0900

    tailwind setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,23 @@
-import { Box } from "@components/models/Box";
-import { ARCanvas } from "@react-three/xr";
 import Link from "next/link";
 import { useState } from "react";
 
 function Home() {
   const [clicked, click] = useState(false);
   return (
-    <div>
+    <div className="m-10 space-y-10 flex flex-col">
       <Link href={"/compare/threejs"}>
         <a>
-          <p>Threejs</p>
+          <p className="text-slate-800 text-3xl">Threejs</p>
         </a>
       </Link>
       <Link href={"/compare/view3d"}>
         <a>
-          <p>View3D (naver)</p>
+          <p className="text-slate-800 text-3xl">View3D (naver)</p>
         </a>
       </Link>
       <Link href={"/compare/model-viewer"}>
         <a>
-          <p>Model-viewer (google)</p>
+          <p className="text-slate-800 text-3xl">Model-viewer (google)</p>
         </a>
       </Link>
       {/* <ARCanvas style={{ width: "auto", height: "90vh" }}>
