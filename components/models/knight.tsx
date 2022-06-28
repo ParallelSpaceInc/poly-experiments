7d37169046a57949b49261e6cb9992dee01d8200
Taehyeong Kim <th.imk3@gmail.com>
2022-06-28 15:00:27 +0900

    import orbit

--- components/models/knight.tsx
+++ components/models/knight.tsx
@@ -1,9 +1,14 @@
+import { useHelper } from "@react-three/drei";
 import { useLoader } from "@react-three/fiber";
+import { useRef } from "react";
+import { BoxHelper } from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 const Knight = () => {
+  const mesh = useRef(null);
+  useHelper(mesh, BoxHelper, "cyan");
   const gltf = useLoader(GLTFLoader, "/knight_gltf/scene.gltf");
-  return <primitive object={gltf.scene} position={[0, 0, 0]} />;
+  return <primitive ref={mesh} object={gltf.scene} position={[0, 0, 0]} />;
 };
 
 export default Knight;
--- pages/compare/threejs.tsx
+++ pages/compare/threejs.tsx
@@ -1,14 +1,16 @@
 import ModelComponent from "@components/models/knight";
+import { OrbitControls } from "@react-three/drei";
 import { ARCanvas } from "@react-three/xr";
 import type { NextPage } from "next";
 
 const Threejs_Demo: NextPage = () => {
   return (
     <div className="h-screen w-screen">
-      <ARCanvas>
+      <ARCanvas camera={{ position: [0, 3, 0] }}>
         <ambientLight />
-        <pointLight position={[5,5,5]} />
+        <pointLight position={[5, 5, 5]} />
         <ModelComponent />
+        <OrbitControls />
       </ARCanvas>
     </div>
   );
