5b1c8efeddb4a96114b6d2dc2a8264dc173a527b
Taehyeong Kim <th.imk3@gmail.com>
2022-06-28 11:12:45 +0900

    make index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,28 @@
 import { Box } from "@components/models/Box";
 import { ARCanvas } from "@react-three/xr";
+import Link from "next/link";
 import { useState } from "react";
 
 function Home() {
   const [clicked, click] = useState(false);
   return (
     <div>
-      <ARCanvas style={{ width: "auto", height: "90vh" }}>
+      <Link href={"/compare/threejs"}>
+        <a>
+          <p>Threejs</p>
+        </a>
+      </Link>
+      <Link href={"/compare/view3d"}>
+        <a>
+          <p>View3D (naver)</p>
+        </a>
+      </Link>
+      <Link href={"/compare/model-viewer"}>
+        <a>
+          <p>Model-viewer (google)</p>
+        </a>
+      </Link>
+      {/* <ARCanvas style={{ width: "auto", height: "90vh" }}>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
         <Box click={click} clicked={clicked} position={[-1.2, 0, 0]} />
@@ -19,7 +35,7 @@ function Home() {
         }}
       >
         click
-      </button>
+      </button> */}
     </div>
   );
 }
