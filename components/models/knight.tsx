e8fb25099a39683b1f7a0ff61676cc2739265956
Taehyeong Kim <th.imk3@gmail.com>
2022-06-28 15:39:51 +0900

    disable shadow

--- components/models/knight.tsx
+++ components/models/knight.tsx
@@ -15,12 +15,11 @@ export const ThreeKnight = () => {
 export const GoogleKnight = () => {
   return (
     <model-viewer
-      style={{ height: "1200px", width: "1200px" }}
+      style={{ height: "1000px", width: "1200px" }}
       src="/knight_gltf/scene.gltf"
       ios-src=""
       poster="https://cdn.glitch.com/36cb8393-65c6-408d-a538-055ada20431b%2Fposter-astronaut.png?v=1599079951717"
       alt="A 3D model of an astronaut"
-      shadow-intensity="1"
       camera-controls
       auto-rotate
       ar
