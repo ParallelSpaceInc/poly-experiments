b6bac61dd8c2b62bc6e1657878ebafcb8023c6b6
Taehyeong Kim <kth@pspace.ai>
2022-07-07 13:50:38 +0900

    show difference btw glb, usdc

--- components/models/mv-knight.tsx
+++ components/models/mv-knight.tsx
@@ -47,8 +47,8 @@ interface ModelViewerElement extends Element {
 const Model = () => (
   <div id="card" className="flex mx-auto h-screen">
     <model-viewer
-      src="/knight_gltf/scene.gltf"
-      ios-src=""
+      src="/MorphStressTest.glb"
+      ios-src="/Grey_Knight/scene.usdc"
       poster="https://cdn.glitch.com/36cb8393-65c6-408d-a538-055ada20431b%2Fposter-astronaut.png?v=1599079951717"
       alt="A 3D model of an astronaut"
       shadow-intensity="1"
